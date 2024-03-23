59d54503b57a6dbcd85890ed357455aa3577df4d
Nabeel Karolia <Nab.kar123@gmail.com>
2024-03-23 06:28:11 +0200

    Added show previews

--- NABKAR303_FTO2308_GroupA2_NabeelKarolia_DWA_Final_Capstone_Project/src/components/Preview.jsx
+++ NABKAR303_FTO2308_GroupA2_NabeelKarolia_DWA_Final_Capstone_Project/src/components/Preview.jsx
@@ -17,17 +17,27 @@ const Preview = () => {
     return <div>Loading...</div>;
   }
 
+  // Function to convert ISO date string to human-readable date
+  const formatUpdatedDate = (dateString) => {
+    const dateObject = new Date(dateString);
+    const month = dateObject.toLocaleString("default", { month: "short" });
+    const day = dateObject.getDate();
+    const year = dateObject.getFullYear();
+    return `${month} ${day}, ${year}`;
+  };
+
   return (
-    <div>
+    <div className="preview-container">
       {shows.map((show, index) => (
-        <div key={index}>
-          <h2>{show.title}</h2>
-          <img src={show.image} alt={show.title} />
-          <p>ID: {show.id}</p>
-          <p>Description: {show.description}</p>
-          <p>Seasons: {show.seasons}</p>
-          <p>Genres: {show.genres.join(", ")}</p>
-          <p>Last Updated: {show.updated}</p>
+        <div className="preview-card" key={index}>
+          <img className="preview-image" src={show.image} alt={show.title} />
+          <div className="preview-content">
+            <h2>{show.title}</h2>
+            <p>Seasons: {show.seasons}</p>
+            <p>Genres: {show.genres.join(", ")}</p>
+            <p>Last Updated: {formatUpdatedDate(show.updated)}</p>
+            <button className="preview-button">Visit Show</button>
+          </div>
         </div>
       ))}
     </div>
