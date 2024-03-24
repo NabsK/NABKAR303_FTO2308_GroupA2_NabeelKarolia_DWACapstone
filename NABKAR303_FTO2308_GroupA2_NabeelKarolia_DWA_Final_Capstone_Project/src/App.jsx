1656bd3e1751ffe7f892b2bc9399e88efcbc990c
Nabeel Karolia <Nab.kar123@gmail.com>
2024-03-24 13:53:19 +0200

    Adjusted HomePage component

--- NABKAR303_FTO2308_GroupA2_NabeelKarolia_DWA_Final_Capstone_Project/src/App.jsx
+++ NABKAR303_FTO2308_GroupA2_NabeelKarolia_DWA_Final_Capstone_Project/src/App.jsx
@@ -1,19 +1,11 @@
-import Carousel from "./components/Carousel";
 import Navbar from "./components/Navbar";
-// import Preview from "./components/Preview";
-// import Genres from "./components/Genres";
-// import Episode from "./components/Episode";
 import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Carousel />
       <HomePage />
-      {/* <Genres /> */}
-      {/* <Preview /> */}
-      {/* <Episode /> */}
     </>
   );
 }
--- NABKAR303_FTO2308_GroupA2_NabeelKarolia_DWA_Final_Capstone_Project/src/components/HomePage.jsx
+++ NABKAR303_FTO2308_GroupA2_NabeelKarolia_DWA_Final_Capstone_Project/src/components/HomePage.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Genres from "./Genres";
 import Preview from "./Preview";
+import Carousel from "./Carousel";
 
 const HomePage = () => {
   const [selectedGenre, setSelectedGenre] = useState(null);
 
   return (
     <div>
+      <Carousel />
       <Genres onSelectGenre={setSelectedGenre} />
       <Preview selectedGenre={selectedGenre} />
     </div>
